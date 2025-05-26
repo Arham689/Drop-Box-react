6030511a1f1b51670e4a01e0727d06794e0c3683
Arham689 <98389773+Arham689@users.noreply.github.com>
2025-05-26 15:51:44 +0530

    first commit

---
+++ src/components/Tagline.tsx
@@ -0,0 +1,13 @@
+import { ArrowRight } from 'lucide-react'
+
+const Tagline = () => {
+  return (
+    <>
+    <div className='w-full h-[56px] flex justify-center underline font-semibold underline-offset-2  fixed top-0  p-4 font-inter text-sm items-center cursor-pointer text-dark-blue bg-light-blue'>
+        Watch our Spring ’25 release event on demand to see how Dash delivers faster, more secure work <ArrowRight size={22}/>
+      </div>
+    </>
+  )
+}
+
+export default Tagline
---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+
+import Header from "../components/Header"
+import Tagline from "../components/Tagline"
+
+const Home = () => {
+  return (
+    <>
+        <div className="relative">
+            {/* tag line */}
+            <Tagline/>
+            {/* heaidng */}
+            <Header/>
+        </div>
+      
+    </>
+  )
+}
+
+export default Home
